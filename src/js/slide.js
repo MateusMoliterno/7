225c708fde46c44c2713765109a8d9e4ceedb4ed
yanndarkz <iansantosmcqueen@gmail.com>
2025-04-02 18:03:16 -0300

    feat: add slide.js

---
+++ src/js/slide.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        300: { slidesPerView: 1.5 },
+        // 640: { slidesPerView: 2 },
+        768: { slidesPerView: 2.3 },
+        1024: { slidesPerView: 3.3 },
+    }
+});
